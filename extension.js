d8f2e0a7e50f334149dc78bcea55976fe3e30368
Ossian Winter <ossian@winter.vg>
2024-06-30 15:42:17 +0200

    Fix formatting

--- extension.js
+++ extension.js
@@ -3,11 +3,11 @@ import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import Gio from 'gi://Gio';
 
 const SCHEDULER_INTERFACE = '<node> \
-    <interface name="com.system76.Scheduler"> \
-          <method name="SetForegroundProcess"> \
-                <arg name="pid" type="u" direction="in" /> \
-          </method> \
-    </interface> \
+  <interface name="com.system76.Scheduler"> \
+    <method name="SetForegroundProcess"> \
+      <arg name="pid" type="u" direction="in" /> \
+    </method> \
+  </interface> \
 </node> \
 ';
 
@@ -15,33 +15,32 @@ const SchedulerProxy = Gio.DBusProxy.makeProxyWrapper(SCHEDULER_INTERFACE);
 const schedulerProxy = new SchedulerProxy(Gio.DBus.system, 'com.system76.Scheduler', '/com/system76/Scheduler');
 
 export default class System76SchedulerExtension extends Extension {
-    enable() {
-		this._foreground = 0;
+  enable() {
+    this._foreground = 0;
 
-		this.handler = global.display.connect('notify::focus-window', () => {
-			const meta_window = global.display.get_focus_window();
-			if (!meta_window) {
-				return;
-			}
+    this.handler = global.display.connect('notify::focus-window', () => {
+      const meta_window = global.display.get_focus_window();
+      if (!meta_window) {
+        return;
+      }
 
-			const pid = meta_window.get_pid();
-			if (!pid || pid === this._foreground) {
-				return;
-			}
+      const pid = meta_window.get_pid();
+      if (!pid || pid === this._foreground) {
+        return;
+      }
 
-			this._foreground = pid;
+      this._foreground = pid;
+      schedulerProxy.SetForegroundProcessRemote(pid, (result, error) => {
+        if (!error) {
+          return;
+        }
 
-			schedulerProxy.SetForegroundProcessRemote(pid, (result, error) => {
-				if (!error) {
-					return;
-				}
+        logError(error);
+      });
+    });
+  }
 
-				logError(error);
-			});
-		});
-	}
-
-    disable() {
-		global.display.disconnect(this._handler);
-    }
+  disable() {
+    global.display.disconnect(this._handler);
+  }
 }
